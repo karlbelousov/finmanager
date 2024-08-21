8cd86cd130475722a8ff3de97a1948eabb61fe70
Karl Belousov <belousovkarl@gmail.com>
2024-08-21 10:58:56 +0600

    поодключил роутер в приложение

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Main } from './components/pages/Main';
+import { Stat } from './components/pages/Stat';
 
-function App(): JSX.Element {
+function App() {
   return (
-    <Main />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/stat' element={<Stat />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
